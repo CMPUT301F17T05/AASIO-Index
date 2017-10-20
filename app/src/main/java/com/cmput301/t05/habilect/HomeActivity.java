d68520629c10985ad1742a9c8a0249b84c16e12d
ioltuszyk <ioltuszy@ualberta.ca>
2017-10-20 00:08:20 -0600

    init home activity, habit dialog

--- app/src/main/java/com/cmput301/t05/habilect/HomeActivity.java
+++ app/src/main/java/com/cmput301/t05/habilect/HomeActivity.java
@@ -1,13 +1,39 @@
 package com.cmput301.t05.habilect;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends FragmentActivity {
+    FragmentManager fragmentManager = getSupportFragmentManager();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        final Button addHabitButton = (Button) findViewById(R.id.addHabitButton);
+        addHabitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final AddHabitDialog addHabitDialog = new AddHabitDialog();
+                addHabitDialog.setOnAddHabitListener(new OnAddHabitListener() {
+                    @Override
+                    public void OnAdded() {
+                        // TODO: implement OnAdded
+                    }
+
+                    @Override
+                    public void OnCancelled() {
+                        // TODO: implement OnCancelled
+                    }
+                });
+                addHabitDialog.show(fragmentManager, "addHabitDialog");
+            }
+        });
     }
 }
