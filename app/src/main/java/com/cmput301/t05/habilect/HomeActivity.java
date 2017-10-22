08e5ae889f87c569ae05316de3b4781305c5090c
ioltuszyk <ioltuszy@ualberta.ca>
2017-10-22 04:42:11 -0600

    Add pager and fragments to home activity

--- app/src/main/java/com/cmput301/t05/habilect/HomeActivity.java
+++ app/src/main/java/com/cmput301/t05/habilect/HomeActivity.java
@@ -1,69 +1,30 @@
 package com.cmput301.t05.habilect;
 
+import android.app.ActionBar;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class HomeActivity extends FragmentActivity {
+public class HomeActivity extends AppCompatActivity {
     FragmentManager fragmentManager = getSupportFragmentManager();
+    ViewPagerHomeAdapter homePagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        final Button addHabitButton = (Button) findViewById(R.id.addHabitButton);
-        addHabitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                final AddHabitDialog addHabitDialog = new AddHabitDialog();
-                addHabitDialog.setOnAddHabitListener(new OnAddHabitListener() {
-                    @Override
-                    public void OnAdded() {
-                        // TODO: implement OnAdded
-                    }
+        homePagerAdapter = new ViewPagerHomeAdapter(fragmentManager);
+        ViewPager mHomeViewPager = (ViewPager) findViewById(R.id.homeViewPager);
+        mHomeViewPager.setAdapter(homePagerAdapter);
 
-                    @Override
-                    public void OnCancelled() {
-                        // TODO: implement OnCancelled
-                    }
-                });
-                addHabitDialog.show(fragmentManager, "addHabitDialog");
-            }
-        });
-
-        final Button viewHabitButton = (Button) findViewById(R.id.viewHabitButton);
-        viewHabitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                final ViewHabitDialog viewHabitDialog = new ViewHabitDialog();
-                viewHabitDialog.setOnViewHabitListener(new OnViewHabitListener() {
-                    @Override
-                    public void OnDeleted() {
-                        // TODO: implement OnDeleted
-                    }
-
-                    @Override
-                    public void OnSaved() {
-                        // TODO: implement OnSaved
-                    }
-
-                    @Override
-                    public void OnFollowed() {
-                        // TODO: implement OnFollowed
-                    }
-
-                    @Override
-                    public void OnCancelled() {
-                        // TODO: implement OnCancelled
-                    }
-                });
-                viewHabitDialog.show(fragmentManager, "viewHabitDialog");
-            }
-        });
     }
 }
---
+++ app/src/main/java/com/cmput301/t05/habilect/ViewPagerHomeAdapter.java
@@ -0,0 +1,34 @@
+package com.cmput301.t05.habilect;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.view.View;
+
+/**
+ * Created by Ian on 2017-10-22.
+ */
+
+public class ViewPagerHomeAdapter extends FragmentPagerAdapter {
+    static final int NUM_PAGES = 2;
+
+    public ViewPagerHomeAdapter(FragmentManager fragmentManager) {
+        super(fragmentManager);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0: return new HomePrimaryFragment();
+            case 1: return new HomeStatisticsFragment();
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return NUM_PAGES;
+    }
+}
+
