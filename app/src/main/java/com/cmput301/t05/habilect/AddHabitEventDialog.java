92c17e1b8af5748b3339df952600514cd5f76d18
OliverRarog <oliverrarog@gmail.com>
2017-11-09 15:24:14 -0700

    Fixed add event dialog sizing

--- app/src/main/java/com/cmput301/t05/habilect/AddHabitEventDialog.java
+++ app/src/main/java/com/cmput301/t05/habilect/AddHabitEventDialog.java
@@ -6,6 +6,8 @@ import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 
 /**
@@ -25,16 +27,20 @@ public class AddHabitEventDialog extends DialogFragment {
         Dialog dialog = getDialog();
         dialog.getWindow().setLayout(
                 ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.MATCH_PARENT
+                ViewGroup.LayoutParams.WRAP_CONTENT
         );
     }
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         final Dialog dialog = new Dialog(getActivity());
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
         LayoutInflater inflater = getActivity().getLayoutInflater();
         View view = inflater.inflate(R.layout.dialog_add_habit_event, null);
         dialog.setContentView(view);
+
+
         return dialog;
     }
 }
