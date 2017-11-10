3c0197fc7bce997c0fa8101daa52f18c076f134c
ioltuszyk <ioltuszy@ualberta.ca>
2017-11-09 18:01:28 -0700

    clean up

--- app/src/main/java/com/cmput301/t05/habilect/HomeActivity.java
+++ app/src/main/java/com/cmput301/t05/habilect/HomeActivity.java
@@ -1,21 +1,14 @@
 package com.cmput301.t05.habilect;
 
-import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 /**
  * @author ioltuszy
