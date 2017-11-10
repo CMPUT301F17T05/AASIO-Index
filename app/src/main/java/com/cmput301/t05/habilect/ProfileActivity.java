5f8f5997f4e24a9d11c135e83a8e3b9ddc8a0c02
ioltuszyk <ioltuszy@ualberta.ca>
2017-11-09 18:02:10 -0700

    modify profile

--- app/src/main/java/com/cmput301/t05/habilect/ProfileActivity.java
+++ app/src/main/java/com/cmput301/t05/habilect/ProfileActivity.java
@@ -1,32 +1,14 @@
 package com.cmput301.t05.habilect;
 
-import android.content.Context;
-import android.content.pm.PackageManager;
 import android.graphics.SurfaceTexture;
 import android.graphics.Typeface;
-import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCaptureSession;
-import android.hardware.camera2.CameraCharacteristics;
-import android.hardware.camera2.CameraDevice;
-import android.hardware.camera2.CameraManager;
 import android.hardware.camera2.CaptureRequest;
-import android.hardware.camera2.params.StreamConfigurationMap;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Size;
-import android.view.Surface;
 import android.view.TextureView;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
 
 /**
  * @author ioltuszy
@@ -38,17 +20,20 @@ public class ProfileActivity extends AppCompatActivity {
     TextureView.SurfaceTextureListener cameraPreviewSurfaceTextureListener = new TextureView.SurfaceTextureListener() {
         @Override
         public void onSurfaceTextureAvailable(SurfaceTexture surfaceTexture, int width, int height) {
-            camera.setup(getApplication() ,width, height);
+            camera.setup(getApplication(), width, height);
             camera.open(getApplication());
         }
+
         @Override
         public void onSurfaceTextureSizeChanged(SurfaceTexture surfaceTexture, int i, int i1) {
 
         }
+
         @Override
         public boolean onSurfaceTextureDestroyed(SurfaceTexture surfaceTexture) {
             return false;
         }
+
         @Override
         public void onSurfaceTextureUpdated(SurfaceTexture surfaceTexture) {
 
@@ -62,10 +47,10 @@ public class ProfileActivity extends AppCompatActivity {
         @Override
         public void onCaptureStarted(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request, long timestamp, long frameNumber) {
             super.onCaptureStarted(session, request, timestamp, frameNumber);
-            if (camera.getSessionStartTime()==0) {
+            if (camera.getSessionStartTime() == 0) {
                 camera.setSessionStartTime(timestamp);
             }
-            camera.getCameraTextureView().setAlpha(MathUtility.EasingOut(timestamp-camera.getSessionStartTime(), 1000000000, 3));
+            camera.getCameraTextureView().setAlpha(MathUtility.EasingOut(timestamp - camera.getSessionStartTime(), 1000000000, 3));
         }
     };
 
