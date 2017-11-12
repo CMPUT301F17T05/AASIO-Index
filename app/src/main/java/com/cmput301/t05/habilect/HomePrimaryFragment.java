1425933a06e9c470a255f8af9c7298f3fdd44ba4
Andrea Whittaker <amwhitta@ualberta.ca>
2017-11-12 16:24:22 -0700

    made ListView on home page work

--- app/src/main/java/com/cmput301/t05/habilect/HomePrimaryFragment.java
+++ app/src/main/java/com/cmput301/t05/habilect/HomePrimaryFragment.java
@@ -1,5 +1,6 @@
 package com.cmput301.t05.habilect;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
@@ -10,7 +11,11 @@ import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+
 import java.util.Date;
 import java.util.ArrayList;
 import java.util.Date;
@@ -33,6 +38,9 @@ import static com.cmput301.t05.habilect.AddHabitEventDialog.REQUEST_IMAGE_CAPTUR
 public class HomePrimaryFragment extends Fragment {
     FragmentManager fragmentManager;
 
+    private ListView habitTypeList;
+    private ArrayList<HabitType> habit_types;
+
     @Override
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -40,6 +48,16 @@ public class HomePrimaryFragment extends Fragment {
                 R.layout.fragment_home_primary, container, false);
 
         fragmentManager = getActivity().getSupportFragmentManager();
+
+        habitTypeList = rootView.findViewById(R.id.incompleteHabitsListView);
+        habitTypeList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(getActivity(), HabitTypeActivity.class);
+                intent.putExtra("ClickedHabitType", habit_types.get(i));
+                startActivity(intent);
+            }
+        });
 
         final Button addHabitButton = (Button) rootView.findViewById(R.id.addHabitButton);
         addHabitButton.setOnClickListener(new View.OnClickListener() {
@@ -118,11 +136,32 @@ public class HomePrimaryFragment extends Fragment {
             }
         });
 
-
-
-
-
         return rootView;
     }
 
+    /**
+     * gets all of the user's habit types locally or from elasticsearch (to be implemented) and
+     * puts them in the ListView
+     *
+     * @see HabitType
+     */
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        habit_types = new ArrayList<>();
+        /*ElasticsearchHabitTypeController.GetHabitTypeTask getHabitTypeTask =
+          new ElasticsearchHabitTypeController.GetHabitTypeTask();
+          habit_types = getHabitTypeTask.execute(null);*/
+        boolean[] plan1 = {true, true, true, true, false, false, false};
+        boolean[] plan2 = {false, false, false, false, false, false, true};
+        HabitType habit1 = new HabitType("clean", "keep the house nice", new Date(), plan1);
+        HabitType habit2 = new HabitType("plan meals", "to save time", new Date(), plan2);
+
+        ArrayAdapter<HabitType> adapter = new ArrayAdapter<HabitType>(getActivity(), R.layout.habit_type_list_item, habit_types);
+        habitTypeList.setAdapter(adapter);
+        habit_types.add(habit1);
+        habit_types.add(habit2);
+        adapter.notifyDataSetChanged();
+    }
 }
