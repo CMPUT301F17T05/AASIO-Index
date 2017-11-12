ab95e2e268c08860b18d50562f7cfce376e8d021
ioltuszyk <ioltuszy@ualberta.ca>
2017-11-12 16:47:11 -0700

    clean up

--- app/src/main/java/com/cmput301/t05/habilect/HomePrimaryFragment.java
+++ app/src/main/java/com/cmput301/t05/habilect/HomePrimaryFragment.java
@@ -7,15 +7,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-<<<<<<< HEAD
-=======
 import java.util.Date;
 import java.util.ArrayList;
 import java.util.Date;
 
 import static android.app.Activity.RESULT_OK;
 import static com.cmput301.t05.habilect.AddHabitEventDialog.REQUEST_IMAGE_CAPTURE;
->>>>>>> origin/MergedTypeAndEvent
 
 /**
  * Shows a list of habit types that can be completed today, allows the user to navigate to
